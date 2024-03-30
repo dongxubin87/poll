c46be79733e5213b928528f7b4ef58d3bdc89d1e
dongxubin87 <165405951+dongxubin87@users.noreply.github.com>
2024-03-30 14:50:35 +0200

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,9 +1,12 @@
 <?php
-$host = 'localhost';
-$dbname = 'myfirstdatabase';
-$dbusername = 'root';
-$dbpassword = '';
-
+// $host = 'localhost';
+// $dbname = 'myfirstdatabase';
+// $dbusername = 'root';
+// $dbpassword = '';
+$host = $_ENV['AZURE_MYSQL_HOST'];
+$dbname = $_ENV['AZURE_MYSQL_DBNAME'];
+$dbusername = $_ENV['AZURE_MYSQL_USERNAME'];
+$dbpassword = $_ENV['AZURE_MYSQL_PASSWORD'];
 try {
   $pdo = new PDO("mysql:host=$host;dbname=$dbname", $dbusername, $dbpassword);
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
